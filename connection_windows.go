ddc92488081f3fb23bd7288c00412e8f974e2c21
konimarti <koni.marti@gmail.com>
2022-10-19 14:32:26 +0200

    add Matrikon automation wrapper
    Fix #36

--- connection_windows.go
+++ connection_windows.go
@@ -218,7 +218,12 @@ func (ao *AutomationObject) Close() {
 
 //NewAutomationObject connects to the COM object based on available wrappers.
 func NewAutomationObject() *AutomationObject {
-	wrappers := []string{"OPC.Automation.1", "Graybox.OPC.DAWrapper.1"}
+	// TODO: list should not be hard-coded
+	wrappers := []string{
+		"OPC.Automation.1",
+		"Graybox.OPC.DAWrapper.1",
+		"Matrikon.OPC.Automation.1",
+	}
 	var err error
 	var unknown *ole.IUnknown
 	for _, wrapper := range wrappers {
